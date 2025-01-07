7d520457e25c9f0b8805b78b2b869d33494ec3cd
jkhiba2023 <khan.jabir1055@gmail.com>
2025-01-08 02:21:41 +0530

    Adding all practice files on repo

---
+++ app.js
@@ -0,0 +1,31 @@
+//Print numbers from 1 to 50 but skip multiples of 3.
+
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 3 === 0) continue;
+//   console.log(i);
+// }
+
+//Print all odd numbers between 1 and 100
+// for (let i = 1; i <= 100; i += 2) {
+//   console.log(i);
+// }
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 2 !== 0) {
+//     console.log(i);
+//   }
+// }
+
+
+// Create a countdown from 10 to 1 using a while loop
+// let count = 10;
+// while (count >= 1) {
+//   console.log(count);
+//   count--;
+// }
+
+
+//Print the square of numbers from 1 to 10
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${i}:${i * i}`);
+// }
