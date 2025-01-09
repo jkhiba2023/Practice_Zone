4e3b2321a27d7c9f39b4c2c03b62ebd97be23857
jkhiba2023 <khan.jabir1055@gmail.com>
2025-01-09 20:06:34 +0530

    Practice code

--- app.js
+++ app.js
@@ -29,3 +29,32 @@
 // for (let i = 1; i <= 10; i++) {
 //   console.log(`${i}:${i * i}`);
 // }
+
+//Reverse an array without using built-in methods
+// using reverse()
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let reverse = arr.reverse();
+// console.log(reverse);
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let reverse = [];
+// for (let i = arr.length - 1; i >= 0; i--) {
+//   reverse.push(arr[i]);
+// }
+// console.log(reverse);
+
+
+//Count how many numbers in an array are greater than 50
+
+// let arr = [10, 55, 75, 20, 90, 45];
+// let count = 0;
+// for (let i = 0; i <= arr.length - 1; i++) {
+//   if (arr[i] > 50) {
+//     console.log(arr[i]);
+//   }
+//   count++;
+// }
+// console.log(`Number greater than 50: ${count}`);
+
+
+
---
+++ function.js
@@ -0,0 +1,6 @@
+for (const i = 0; i < 5; i++) {
+  const msg = 'Hello World!!!';
+  console.log(msg);
+
+}
+console.log(msg);
