dd7c1d10db1156c2e9431880b4fe5327fe4375ca
jkhiba2023 <khan.jabir1055@gmail.com>
2025-01-14 14:47:38 +0530

    Practicing array

--- function.js
+++ function.js
@@ -1,16 +1,4 @@
-function function_name1() {
-  console.log("Inside function_name1");
+let colors = ["Red", "Blue", "Green", "Yellow"];
+console.log(colors);
+console.log(typeof colors);
 
-  function function_name2() {
-    console.log("Inside function_name2");
-
-    function function_name3() {
-      console.log("Inside function_name3");
-    }
-    return function_name3; // Return function_name3
-  }
-  return function_name2; // Return function_name2
-
-
-}
-function_name1();
