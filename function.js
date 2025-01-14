e10c5b524587295fe66ff13fafeae1a735a84c30
jkhiba2023 <khan.jabir1055@gmail.com>
2025-01-14 15:16:44 +0530

    Playing with array modification methods

--- function.js
+++ function.js
@@ -1,11 +1,29 @@
-let colors = ['rad', 'lue', 'ello', 'blue'];
-console.log(colors);
-colors[0] = 'red';
-console.log(colors);
-colors[1] = 'black';
-console.log(colors);
-colors[2] = 'yellow';
-console.log(colors);
+let upcomingMovies = ['Fateh', 'Deva'];
+console.log(upcomingMovies);
+upcomingMovies.push('Game Changers');
+console.log(upcomingMovies);
+upcomingMovies.push('Marco');
+upcomingMovies.push('Welcome to the jungle');
+upcomingMovies.push('Sikandar')
+upcomingMovies.push("pushpa2");
+console.log(upcomingMovies);
+upcomingMovies.unshift('Sky Force');
+upcomingMovies.unshift('Badass Ravikumar');
+console.log(upcomingMovies);
+let remove = upcomingMovies.shift();
+console.log(upcomingMovies);
+console.log(remove);
+remove = upcomingMovies.shift();
+console.log(remove);
+remove = upcomingMovies.pop();
+console.log(remove);
+console.log(upcomingMovies);
+
+
+
+
+
+
 
 
 
