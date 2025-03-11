06eb00bd4a8e96eeea1e3dd2263cfdbc8f15b9d8
jkhiba2023 <khan.jabir1055@gmail.com>
2025-03-12 04:04:58 +0530

    JavaScript Program to Add Two Numbers

--- function.js
+++ function.js
@@ -1,2 +1,8 @@
-//JavaScript Program To Print Hello World
+//1. JavaScript Program To Print Hello World
 console.log("Hello World");
+
+//2. JavaScript Program to Add Two Numbers
+let a = 10;
+let b = 5;
+let result = a + b;
+console.log(result); 
