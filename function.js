4e6c94d3b8aa9a181fc1d1bd7fe62b9088b92058
jkhiba2023 <khan.jabir1055@gmail.com>
2025-03-12 04:15:25 +0530

    JavaScript Program to Swap Two Variables

--- function.js
+++ function.js
@@ -11,3 +11,14 @@ function adding(c, d) {
   return c + d;
 }
 console.log(adding(20, 30));
+
+//3. JavaScript Program to Calculate the Area of a Triangle
+
+function areaOfTriangle(base, height) {
+  let result = base * height * 0.5;
+  return result;
+}
+
+console.log(areaOfTriangle(3.5, 4.2));
+
+//4. JavaScript Program to Swap Two Variables
