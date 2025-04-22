b2f2b1182e3f1906c970776732b549f7b10a08d6
jkhiba2023 <khan.jabir1055@gmail.com>
2025-04-22 18:19:34 +0530

    Getting largest number

--- Day1.js
+++ Day1.js
@@ -2,80 +2,80 @@
 //1.JavaScript Program To Print Hello World
 
 //   Approach 1
-function msg(str) {
-  return str
-}
+// function msg(str) {
+//   return str
+// }
 
-let str = "Hello world";
-console.log(msg(str));
+// let str = "Hello world";
+// console.log(msg(str));
 
-//  Approach 2
+// //  Approach 2
 
-console.log('Hello World!!!!');
+// console.log('Hello World!!!!');
 
-//2. JavaScript Program to Calculate the Area of a Triangle
+// //2. JavaScript Program to Calculate the Area of a Triangle
 
-// Approach 1
+// // Approach 1
 
-function areaOfTriangle(base, height) {
-  let area = 0;
-  const value = 0.5;
-  area = value * base * height;
-  return area
-}
+// function areaOfTriangle(base, height) {
+//   let area = 0;
+//   const value = 0.5;
+//   area = value * base * height;
+//   return area
+// }
 
-let base = 15;
-let height = 25;
-console.log(areaOfTriangle(base, height));
+// let base = 15;
+// let height = 25;
+// console.log(areaOfTriangle(base, height));
 
-// Approach 2
+// // Approach 2
 
-let base1 = 49;
-let height1 = 89.5;
-const value = 0.5;
-let AT = 0;
-AT = value * base1 * height1;
-console.log(AT);
+// let base1 = 49;
+// let height1 = 89.5;
+// const value = 0.5;
+// let AT = 0;
+// AT = value * base1 * height1;
+// console.log(AT);
 
-//3. JavaScript Program to Swap Two Variables
+// //3. JavaScript Program to Swap Two Variables
 
-// Approach 1
+// // Approach 1
 
-let a = 15;
-let b = 25;
-let temp;
-console.log(`Value of a before swapping ${a}`);
-console.log(`Value of b before swapping ${b}`);
+// let a = 15;
+// let b = 25;
+// let temp;
+// console.log(`Value of a before swapping ${a}`);
+// console.log(`Value of b before swapping ${b}`);
 
-temp = a;
-a = b;
-b = temp;
-console.log(`Value of a after swapping ${a}`);
-console.log(`Value of b after swapping ${b}`);
+// temp = a;
+// a = b;
+// b = temp;
+// console.log(`Value of a after swapping ${a}`);
+// console.log(`Value of b after swapping ${b}`);
 
-// Approach 2
+// // Approach 2
 
-let a1 = 20;
-let b1 = 15;
-console.log(`Value of a1 before swapping is ${a1}`);
-console.log(`Value of b1 before swapping is ${b1}`);
+// let a1 = 20;
+// let b1 = 15;
+// console.log(`Value of a1 before swapping is ${a1}`);
+// console.log(`Value of b1 before swapping is ${b1}`);
 
-a1 = a1 + b1;
-b1 = a1 - b1;
-a1 = a1 - b1;
-console.log(`Value of a1 after swapping is ${a1}`);
-console.log(`Value of b1 after swapping is ${b1}`);
+// a1 = a1 + b1;
+// b1 = a1 - b1;
+// a1 = a1 - b1;
+// console.log(`Value of a1 after swapping is ${a1}`);
+// console.log(`Value of b1 after swapping is ${b1}`);
 
-//Approach 3
+// //Approach 3
 
-let a2 = 25;
-let b2 = 35;
-console.log(`Value of a2 before swapping is ${a2}`);
-console.log(`Value of b2 before swapping is ${b2}`);
+// let a2 = 25;
+// let b2 = 35;
+// console.log(`Value of a2 before swapping is ${a2}`);
+// console.log(`Value of b2 before swapping is ${b2}`);
 
-[a2, b2] = [b2, a2]
-console.log(`Value of a2 after swapping is ${a2}`);
-console.log(`Value of b2 after swapping is ${b2}`);
+// [a2, b2] = [b2, a2]
+// console.log(`Value of a2 after swapping is ${a2}`);
+// console.log(`Value of b2 after swapping is ${b2}`);
 
 
 
---
+++ Day2.js
@@ -0,0 +1,6 @@
+//1. Find Largest Element in Array
+
+//Approch 1
+const arr = [10, 27, 15, 18, 11]
+const lElement = Math.max(...arr);
+console.log(lElement);
