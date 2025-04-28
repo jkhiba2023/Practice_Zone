2602efcce094967fbf2b3ca479666b9aeec6bc41
jkhiba2023 <khan.jabir1055@gmail.com>
2025-04-28 23:18:09 +0530

    Check Even Odd Numbers

--- Day3.js
+++ Day3.js
@@ -6,3 +6,38 @@
 
 // let result = 10
 // console.log(generateRandom(result));
+
+
+//2. Javascript Program to Check if a number is Positive, Negative, or Zero
+
+// function checkNumber(num) {
+//   if (num < 0) {
+//     console.log(`Number is negative`);
+//   } else if (num > 0) {
+//     console.log(`Number is positive`);
+
+//   } else {
+//     console.log(`Number is zero`);
+
+//   }
+// }
+
+// console.log(checkNumber(-1));
+// console.log(checkNumber(-12));
+// console.log(checkNumber(1));
+// console.log(checkNumber(0));
+
+
+//3. Javascript Program to Check if a Number is Odd or Even
+
+// function checkEvenOdd(num) {
+//   if (num % 2 === 0) {
+//     console.log(`Even Number`);
+//   } else {
+//     console.log(`Odd Number`);
+//   }
+// }
+
+// console.log(checkEvenOdd(15));
+// console.log(checkEvenOdd(14));
+
