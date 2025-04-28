cbabb573254bc68b6028d2c9bdf9ad05cdae80dc
jkhiba2023 <khan.jabir1055@gmail.com>
2025-04-28 23:01:15 +0530

    Generate Random Numbers

---
+++ Day3.js
@@ -0,0 +1,8 @@
+//1. Javascript Program to Generate a Random Number
+
+// function generateRandom(n) {
+//   return Math.floor(Math.random() * 10)
+// }
+
+// let result = 10
+// console.log(generateRandom(result));
