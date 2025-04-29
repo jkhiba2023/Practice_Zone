abc42f8685f68a329871192e8f7b1c3ea83f9103
jkhiba2023 <khan.jabir1055@gmail.com>
2025-04-29 21:06:57 +0530

    Convert KM to Miles

---
+++ Day4.js
@@ -0,0 +1,14 @@
+//1. JavaScript Program to Convert Kilometers to Miles.
+
+function convertKMtoM(kilometer) {
+  return kilometer * 0.621371;
+}
+
+let kilometer1 = 16;
+let miles1 = convertKMtoM(kilometer1);
+console.log(miles1);
+
+let kilometer2 = 18;
+let miles2 = convertKMtoM(kilometer2)
+console.log(miles2);
+
