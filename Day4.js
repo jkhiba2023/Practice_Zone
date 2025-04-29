9f6e3e784f86c708c401af2b957f71b87cab9ef1
jkhiba2023 <khan.jabir1055@gmail.com>
2025-04-29 21:19:40 +0530

    Convert Celcius to Fahrenheit

--- Day4.js
+++ Day4.js
@@ -1,14 +1,33 @@
 //1. JavaScript Program to Convert Kilometers to Miles.
 
-function convertKMtoM(kilometer) {
-  return kilometer * 0.621371;
-}
+// function convertKMtoM(kilometer) {
+//   return kilometer * 0.621371;
+// }
 
-let kilometer1 = 16;
-let miles1 = convertKMtoM(kilometer1);
-console.log(miles1);
+// let kilometer1 = 16;
+// let miles1 = convertKMtoM(kilometer1);
+// console.log(miles1);
 
-let kilometer2 = 18;
-let miles2 = convertKMtoM(kilometer2)
-console.log(miles2);
+// let kilometer2 = 18;
+// let miles2 = convertKMtoM(kilometer2)
+// console.log(miles2);
+
+//2. JavaScript Program to Convert Celsius to Fahrenheit.
+
+// function convertCToF(celsius) {
+//   return (celsius * 9 / 5) + 32;
+// }
+
+// let celsius1 = 20;
+// const fahrenheit1 = convertCToF(celsius1);
+// console.log(fahrenheit1);
+
+// let celsius2 = 30;
+// const fahrenheit2 = convertCToF(celsius2)
+// console.log(fahrenheit2);
+
+//3. JavaScript .
+
+
+
 
