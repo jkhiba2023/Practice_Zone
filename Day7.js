7e3e1d8bbb0e246d492cb26cd896b7c8f9fb82f2
jkhiba2023 <khan.jabir1055@gmail.com>
2025-05-14 18:17:26 +0530

    Arrange the array number in descending order

--- Day7.js
+++ Day7.js
@@ -4,5 +4,11 @@
 
 let arr1 = [10, 99, 12, 10, 25, 13, 68, 99, 17, 20, 25, 23, 24];
 console.log(arr1.length);
-let unique = [...new Set(arr1)];
+let arr2 = [35, 40, 45, 99, 32, 15, 18, 58, 25, 68];
+let margeArr = arr1.concat(arr2)
+console.log(margeArr.length);
+console.log(margeArr.sort((a, b) => b - a));
+
+
+let unique = [...new Set(margeArr)];
 console.log(unique);
