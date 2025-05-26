c3fb050a2ed3b1aadbcc0f69e4922fcdc726a2e5
jkhiba2023 <khan.jabir1055@gmail.com>
2025-05-26 16:47:40 +0530

    Finding Largest Number using Math.max()

--- Day9.js
+++ Day9.js
@@ -1,17 +1,27 @@
 //JavaScript Program to Find the Largest Among Three Numbers.
-//1st Approach
 
+//1st Approach
 // Using if-else condition.
 
-function findLargest(num1, num2, num3) {
-  if (num1 >= num2 && num1 >= num3) {
-    return num1;
-  } else if (num2 >= num1 && num2 >= num3) {
-    return num2;
-  } else {
-    return num3;
-  }
-}
+// function findLargest(num1, num2, num3) {
+//   if (num1 >= num2 && num1 >= num3) {
+//     return num1;
+//   } else if (num2 >= num1 && num2 >= num3) {
+//     return num2;
+//   } else {
+//     return num3;
+//   }
+// }
 
-const largestNum = findLargest(24, 36, 30);
-console.log(largestNum);
+// const largestNum = findLargest(24, 36, 30);
+// console.log(largestNum);
+
+//2nd Approach
+//Using Math.max()
+
+// function findLargest(num1, num2, num3) {
+//   return Math.max(num1, num2, num3);
+// }
+
+// const largestNumber = findLargest(29, 49, 19);
+// console.log(largestNumber);
