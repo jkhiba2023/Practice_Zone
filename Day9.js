4092170322a4520fb74eeff59e5ea23878333385
jkhiba2023 <khan.jabir1055@gmail.com>
2025-05-30 01:38:09 +0530

    Using Ternary Operator

--- Day9.js
+++ Day9.js
@@ -29,9 +29,19 @@
 //3rd Approach
 //Using spread operator using Math.max()
 
+// function findLargest(num1, num2, num3) {
+//   return Math.max(...[num1, num2, num3]);
+// }
+
+// const largestNumber = findLargest(15, 19, 11);
+// console.log(largestNumber);
+
+//4th Approach
+//Using Ternary Operator
+
 function findLargest(num1, num2, num3) {
-  return Math.max(...[num1, num2, num3]);
+  return num1 >= num2 && num1 >= num3 ? num1 : num2 >= num1 && num2 >= num3 ? num2 : num3;
 }
 
-const largestNumber = findLargest(15, 19, 11);
-console.log(largestNumber);
+const maxNum = findLargest(2, 8, 6);
+console.log(maxNum);
